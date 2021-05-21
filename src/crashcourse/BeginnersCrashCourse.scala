5175d48bf45438000a6002e2da36fd434bd87f3f
tothetop430 <jhuseyrer355545@gmail.com>
2021-05-21 21:48:57 +0530

    update BeginnersCrashCourse

---
+++ src/crashcourse/BeginnersCrashCourse.scala
@@ -0,0 +1,16 @@
+package crashcourse
+
+object BeginnersCrashCourse extends App {
+  var var1 = 1;  //Mutable
+  val var2 = 2; //Immutable
+
+  val bool: Boolean = true  //or false
+  val chr: Char = 'a'   //Make sure you use single quotes for character type value
+  val str: String = "String"    //And double quotes for a string type
+  val int: Int = 123    //A int value
+  val shrt: Short = 123   //A short value
+  val lng: Long = 123     //A long value
+  val lng2 = 123L   //Make sure to put L to let compiler know that it is of type Long
+  val flt: Float = 123.0f   //Make sure to put f at the end of float values
+  val dbl: Double = 123.0   //Otherwise compiler will infer it as a type of double
+}
